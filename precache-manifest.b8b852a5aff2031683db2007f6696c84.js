9217e2f28900cb3dab4476b6843536df4546de80
Atul Kaushik <kaushikatul033@gmail.com>
2021-04-25 13:27:48 +0530

    Updates

---
+++ precache-manifest.b8b852a5aff2031683db2007f6696c84.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4374f93e4b8bd4971c9a57ce8dd9ac",
+    "url": "/weatherappV1/index.html"
+  },
+  {
+    "revision": "19d3c7a7eb416c475af2",
+    "url": "/weatherappV1/static/css/main.02be3142.chunk.css"
+  },
+  {
+    "revision": "d9eeac182283b4d59ede",
+    "url": "/weatherappV1/static/js/2.e59111a6.chunk.js"
+  },
+  {
+    "revision": "f066fac514e06467b7e5320bf7e4f390",
+    "url": "/weatherappV1/static/js/2.e59111a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96443cd85b1c96153fb0",
+    "url": "/weatherappV1/static/js/3.eedf8376.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherappV1/static/js/3.eedf8376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbca52b69519e01b4f10",
+    "url": "/weatherappV1/static/js/4.b4b99463.chunk.js"
+  },
+  {
+    "revision": "19d3c7a7eb416c475af2",
+    "url": "/weatherappV1/static/js/main.f25b2983.chunk.js"
+  },
+  {
+    "revision": "7e112eb023c4ba2d1a17",
+    "url": "/weatherappV1/static/js/runtime-main.5060c0d1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.08cf26c3e91e201971b6e8169e27da6d.js"
+  "/weatherappV1/precache-manifest.b8b852a5aff2031683db2007f6696c84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weatherappV1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
